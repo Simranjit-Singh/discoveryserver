b9cbe58e4777e9623cb3dc4c4f8c3227514591ef
ssimranj1 <`12345QWERTqwert`12345QWERTqwert>
2021-01-10 16:05:07 -0500

    Test CDI

--- src/main/java/com/microservice/discoveryserver/DiscoveryserverApplication.java
+++ src/main/java/com/microservice/discoveryserver/DiscoveryserverApplication.java
@@ -11,5 +11,5 @@ public class DiscoveryserverApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryserverApplication.class, args);
     }
-
+//TestCDIi.................
 }
